441f6c01c68ee6f488630d6856cbbfecfe0951d4
zhirnovaAlisA <zirnovaalisa277@gmail.com>
2025-01-16 14:34:55 +0300

    imports, exports

---
+++ app.js
@@ -0,0 +1,6 @@
+import Character from "./domain";
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game';
+
+const game = new Game();
+game.start();
+
